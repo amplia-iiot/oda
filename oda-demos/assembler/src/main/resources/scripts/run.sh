fad8ad658b80a59cb3afcbc7adb790be57bd7972
Francisco Javier de Arcos <javier.dearcos@amplia.es>
2018-10-18 15:00:36 +0200

    Add ODA assembler to demos

---
+++ oda-demos/assembler/src/main/resources/scripts/run.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+# Launch OpenGate Device Agent
+# Use -d flag to debug
+
+cd $(dirname $0)
+cd ..
+
+# Port to debug
+DEBUG_PORT=8000
+
+PARAMS=" -Dorg.ops4j.pax.logging.DefaultServiceLog.level=WARN"
+
+if [ "$1" = "-d" ]
+then
+    PARAMS="$PARAMS -agentlib:jdwp=transport=dt_socket,server=y,address=$DEBUG_PORT,suspend=y"
+fi
+
+java $PARAMS -cp 'bin/*' org.apache.felix.main.Main
