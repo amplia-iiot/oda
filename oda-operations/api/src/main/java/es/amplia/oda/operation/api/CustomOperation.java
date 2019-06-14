af0254e060905040ea9b1e16277f46e05a662e9a
Francisco Javier de Arcos <javier.dearcos@amplia.es>
2019-06-14 08:45:50 +0200

    [ODA-204] Modify Custom Operation API to add steps to the operation result

--- oda-operations/api/src/main/java/es/amplia/oda/operation/api/CustomOperation.java
+++ oda-operations/api/src/main/java/es/amplia/oda/operation/api/CustomOperation.java
@@ -2,6 +2,7 @@ package es.amplia.oda.operation.api;
 
 import lombok.Value;
 
+import java.util.Collection;
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 
@@ -10,14 +11,31 @@ public interface CustomOperation {
     @Value
     class Result
     {
-        Status status;
-        String description;
+        private Status status;
+        private String description;
+        private Collection<Step> steps;
+
     }
 
     enum Status {
         SUCCESSFUL,
         ERROR_IN_PARAM,
         ERROR_PROCESSING
+    }
+
+    @Value
+    class Step {
+        private String name;
+        private StepStatus result;
+        private String description;
+        private long timestamp;
+    }
+
+    enum StepStatus {
+        SUCCESSFUL,
+        ERROR,
+        SKIPPED,
+        NOT_EXECUTED
     }
 
     CompletableFuture<Result> execute(String deviceId, Map<String, Object> params);
