6c9b4eb15757bdf70bdb7edfc3b1d17ac5744fe0
Francisco Javier de Arcos <javier.dearcos@amplia.es>
2019-07-10 08:40:26 +0200

    [ODA-213] Fix problem registering commands or events in ATServer before configuring it

--- oda-hardware/atserver/src/main/java/es/amplia/oda/hardware/atserver/Activator.java
+++ oda-hardware/atserver/src/main/java/es/amplia/oda/hardware/atserver/Activator.java
@@ -2,15 +2,14 @@ package es.amplia.oda.hardware.atserver;
 
 import es.amplia.oda.core.commons.utils.ConfigurableBundle;
 import es.amplia.oda.core.commons.utils.ConfigurableBundleImpl;
+import es.amplia.oda.core.commons.utils.ServiceRegistrationManager;
+import es.amplia.oda.core.commons.utils.ServiceRegistrationManagerOsgi;
 import es.amplia.oda.hardware.atmanager.api.ATManager;
 
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
-import org.osgi.framework.ServiceRegistration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.util.Collections;
 
 public class Activator implements BundleActivator {
 
@@ -18,17 +17,16 @@ public class Activator implements BundleActivator {
 
     private ATServer atServer;
     private ConfigurableBundle configurableBundle;
-    private ServiceRegistration<ATManager> atManagerRegistration;
 
     @Override
     public void start(BundleContext bundleContext) {
         LOGGER.info("AT Server starting");
 
-        atServer = new ATServer();
-        atManagerRegistration = bundleContext.registerService(ATManager.class, atServer, null);
+        ServiceRegistrationManager<ATManager> atManagerRegistrationManager =
+                new ServiceRegistrationManagerOsgi<>(bundleContext, ATManager.class);
+        atServer = new ATServer(atManagerRegistrationManager);
         ATServerConfigurationUpdateHandler configHandler = new ATServerConfigurationUpdateHandler(atServer);
-        configurableBundle = new ConfigurableBundleImpl(bundleContext, configHandler,
-                Collections.singletonList(atManagerRegistration));
+        configurableBundle = new ConfigurableBundleImpl(bundleContext, configHandler);
 
         LOGGER.info("AT Server started");
     }
@@ -37,7 +35,6 @@ public class Activator implements BundleActivator {
     public void stop(BundleContext context) {
         LOGGER.info("AT Server stopping");
 
-        atManagerRegistration.unregister();
         configurableBundle.close();
         atServer.close();
 
