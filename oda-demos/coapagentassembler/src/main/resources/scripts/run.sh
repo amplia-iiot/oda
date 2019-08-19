1051a062e4d0ccd5686356fb902459edbc9d6020
Francisco Javier de Arcos <javier.dearcos@amplia.es>
2019-08-19 14:02:31 +0200

    Update run script and bundle configuration files

--- oda-demos/coapagentassembler/src/main/resources/scripts/run.sh
+++ oda-demos/coapagentassembler/src/main/resources/scripts/run.sh
@@ -8,11 +8,11 @@ cd ..
 # Port to debug
 DEBUG_PORT=8000
 
-PARAMS=" -Dorg.ops4j.pax.logging.DefaultServiceLog.level=WARN"
+PARAMS=" -Djava.net.preferIPv4Stack=true -Djava.security.policy=security/dio.policy -Dlogback.configurationFile=conf/logback.xml"
 
 if [ "$1" = "-d" ]
 then
     PARAMS="$PARAMS -agentlib:jdwp=transport=dt_socket,server=y,address=$DEBUG_PORT,suspend=y"
 fi
 
-java $PARAMS -cp 'bin/*' -Dlogback.configurationFile=conf/logback.xml org.apache.felix.main.Main
+java ${PARAMS} -cp 'bin/*' org.apache.felix.main.Main
