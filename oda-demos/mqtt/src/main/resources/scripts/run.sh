957429e03191915b0e3169906d9078d90f4062e6
Adrian Menendez <adrian.menendez@amplia.es>
2019-10-08 12:42:15 +0200

    Fix in Mqtt demo

--- oda-demos/mqtt/src/main/resources/scripts/run.sh
+++ oda-demos/mqtt/src/main/resources/scripts/run.sh
@@ -8,11 +8,11 @@ cd ..
 # Port to debug
 DEBUG_PORT=8000
 
-PARAMS=" -Djava.net.preferIPv4Stack=true -Djava.security.policy=security/dio.policy -Dlogback.configurationFile=conf/logback.xml"
+PARAMS=" -Djava.net.preferIPv4Stack=true -Djava.security.policy=security/dio.policy -Dlogback.configurationFile=conf/logback.xml -Dgosh.args=--nointeractive -Dorg.jline.terminal.dumb=true"
 
 if [ "$1" = "-d" ]
 then
     PARAMS="$PARAMS -agentlib:jdwp=transport=dt_socket,server=y,address=$DEBUG_PORT,suspend=y"
 fi
 
-java ${PARAMS} -cp 'bin/*' org.apache.felix.main.Main
+java ${PARAMS} -cp 'bin/*' org.apache.felix.main.Main &
