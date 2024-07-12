28ec15d141467a45d8baaf2d1e5b05429d0bca4c
rromano <ruben.romano@amplia.es>
2024-07-12 12:09:36 +0200

    [ASRSG-995] -  add new ftp bundle

---
+++ oda-hardware/ftp/src/main/java/es/amplia/oda/hardware/ftp/FtpClient.java
@@ -0,0 +1,110 @@
+package es.amplia.oda.hardware.ftp;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.net.ProtocolCommandEvent;
+import org.apache.commons.net.ProtocolCommandListener;
+import org.apache.commons.net.ftp.*;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Slf4j
+public class FtpClient {
+
+    private final String server;
+    private final int port;
+    private final String user;
+    private final String password;
+    private final boolean passiveMode;
+    private FTPClient ftpClient;
+
+    public FtpClient(String server, int port, String user, String password, boolean passiveMode) {
+        this.server = server;
+        this.port = port;
+        this.user = user;
+        this.password = password;
+        this.passiveMode = passiveMode;
+    }
+
+    public void connect() throws IOException {
+        ftpClient = new FTPClient();
+
+        // add this to redirect ftp commands and answers to log
+        ftpClient.addProtocolCommandListener(new ProtocolCommandListener() {
+            @Override
+            public void protocolCommandSent(ProtocolCommandEvent protocolCommandEvent) {
+                log.trace("{} - {}", protocolCommandEvent.getCommand(), protocolCommandEvent.getMessage());
+            }
+
+            @Override
+            public void protocolReplyReceived(ProtocolCommandEvent protocolCommandEvent) {
+                log.trace("Reply received : {}", protocolCommandEvent.getMessage());
+            }
+        });
+
+        // configuration for ftp server to be unix
+        ftpClient.configure(new FTPClientConfig(FTPClientConfig.SYST_UNIX));
+
+        // connect to ftp
+        ftpClient.connect(server, port);
+        if (!FTPReply.isPositiveCompletion(ftpClient.getReplyCode())) {
+            ftpClient.disconnect();
+            log.error("Error connecting to FTP server {}", this.server);
+        }
+
+        // login in ftp server
+        ftpClient.login(user, password);
+
+        // set file type for downloads (in some servers must be done after logging)
+        ftpClient.setFileType(FTP.BINARY_FILE_TYPE);
+
+        if(this.passiveMode) {
+            ftpClient.enterLocalPassiveMode();
+        }
+    }
+
+    public void disconnect() throws IOException {
+        ftpClient.disconnect();
+    }
+
+    public List<FtpFile> listFiles(String path) throws IOException {
+        FTPFile[] files = ftpClient.listFiles(path);
+        return Arrays.stream(files)
+                .map(this::mapFtpFile)
+                .collect(Collectors.toList());
+    }
+
+    public void changeDir(String path) throws IOException {
+        boolean result = ftpClient.changeWorkingDirectory(path);
+        if (!result) {
+            log.error("Error changing working directory in Ftp server");
+        }
+    }
+
+    public void downloadFileKeepDate(FtpFile source, String destination) throws IOException {
+        FileOutputStream out = new FileOutputStream(destination);
+        ftpClient.retrieveFile(source.getName(), out);
+        out.close();
+
+        // update timestamp of the downloaded file to math the one in the server
+        new File(destination).setLastModified(source.getTimestamp());
+    }
+
+    public void downloadFile(String source, String destination) throws IOException {
+        FileOutputStream out = new FileOutputStream(destination);
+        ftpClient.retrieveFile(source, out);
+        out.close();
+    }
+
+
+    private FtpFile mapFtpFile(FTPFile fileFromFtpServer) {
+        FtpFile newFtpFile = new FtpFile();
+        newFtpFile.setName(fileFromFtpServer.getName());
+        newFtpFile.setTimestamp(fileFromFtpServer.getTimestamp().getTimeInMillis());
+        return newFtpFile;
+    }
+}
---
+++ oda-hardware/ftp/src/main/java/es/amplia/oda/hardware/ftp/FtpFile.java
@@ -0,0 +1,9 @@
+package es.amplia.oda.hardware.ftp;
+
+import lombok.Data;
+
+@Data
+public class FtpFile {
+    String name;
+    long timestamp;
+}
