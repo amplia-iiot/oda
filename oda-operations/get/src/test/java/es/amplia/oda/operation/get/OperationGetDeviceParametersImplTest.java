2c4ae7ee0193ed189c02b8a0c5997489a742cd3b
jgarcia <juanjose.garcia@amplia.es>
2025-05-29 15:19:53 +0200

    [ODA-497] La operación GET ejecuta los datastreamGetters en lugar de ir al StateManager

--- oda-operations/get/src/test/java/es/amplia/oda/operation/get/OperationGetDeviceParametersImplTest.java
+++ oda-operations/get/src/test/java/es/amplia/oda/operation/get/OperationGetDeviceParametersImplTest.java
@@ -1,8 +1,11 @@
 package es.amplia.oda.operation.get;
 
+import es.amplia.oda.core.commons.interfaces.DatastreamsGetter;
+import es.amplia.oda.core.commons.interfaces.DatastreamsGetter.CollectedValue;
 import es.amplia.oda.core.commons.interfaces.StateManager;
 import es.amplia.oda.operation.api.OperationGetDeviceParameters;
-import es.amplia.oda.core.commons.utils.DatastreamValue;
+import es.amplia.oda.core.commons.utils.DatastreamsGettersFinder;
+import es.amplia.oda.core.commons.utils.Event;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -11,6 +14,7 @@ import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -24,8 +28,13 @@ import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class OperationGetDeviceParametersImplTest {
+    @SafeVarargs
+    private static <T> Set<T> asSet(T... ts) {
+        return new HashSet<>(Arrays.asList(ts));
+    }
 
     private static final String TEST_DEVICE_ID = "testDevice";
+    private static final List<String> TEST_DEVICE_ID_LIST = Collections.singletonList(TEST_DEVICE_ID);
     private static final String TEST_FEED = "testFeed";
     private static final String TEST_DATASTREAM_ID_1 = "d1";
     private static final String TEST_VALUE_1 = "Hello";
@@ -34,27 +43,28 @@ public class OperationGetDeviceParametersImplTest {
     private static final double TEST_VALUE_2 = 12.34;
     private static final long TEST_AT_2 = System.currentTimeMillis() - 3600000;
     private static final String TEST_DATASTREAM_ID_3 = "d3";
-    private static final long TEST_AT_3 = System.currentTimeMillis() + 3600000;
     private static final String TEST_DATASTREAM_ID_4 = "d4";
     private static final long TEST_AT_4 = System.currentTimeMillis() - 86400000;
 
     private static final Set<String> TEST_DATASTREAMS = new HashSet<>(
             Arrays.asList(TEST_DATASTREAM_ID_1, TEST_DATASTREAM_ID_2, TEST_DATASTREAM_ID_3, TEST_DATASTREAM_ID_4)
     );
-    private static final long TEST_AT = System.currentTimeMillis();
-    private static final String TEST_ERROR = "Error!";
-    private static final Set<DatastreamValue> TEST_DATASTREAM_VALUES = new HashSet<>(
-            Arrays.asList(
-                    new DatastreamValue(TEST_DEVICE_ID, TEST_DATASTREAM_ID_1, null, TEST_AT_1, TEST_VALUE_1, DatastreamValue.Status.OK, null, false, false),
-                    new DatastreamValue(TEST_DEVICE_ID, TEST_DATASTREAM_ID_2, null, TEST_AT_2, TEST_VALUE_2, DatastreamValue.Status.OK, null, false, false),
-                    new DatastreamValue(TEST_DEVICE_ID, TEST_DATASTREAM_ID_3, TEST_FEED, TEST_AT_3, null, DatastreamValue.Status.NOT_FOUND, null, false, false),
-                    new DatastreamValue(TEST_DEVICE_ID, TEST_DATASTREAM_ID_4, TEST_FEED, TEST_AT_4, null, DatastreamValue.Status.PROCESSING_ERROR, TEST_ERROR, false, false)
-            )
+    private static final String TEST_ERROR = "One of these is null : deviceId '" + TEST_DEVICE_ID + "', datastreamId '" + TEST_DATASTREAM_ID_4 + "' or value 'null'.";
+    private static final List<Event> TEST_EVENTS_VALUES = Arrays.asList(
+        new Event(TEST_DATASTREAM_ID_1, TEST_DEVICE_ID, null, null, TEST_AT_1, TEST_VALUE_1),
+        new Event(TEST_DATASTREAM_ID_2, TEST_DEVICE_ID, null, null, TEST_AT_2, TEST_VALUE_2)
     );
 
-
+    @Mock
+    private DatastreamsGetter getterForId1;
+    @Mock
+    private DatastreamsGetter getterForId2;
+    @Mock
+    private DatastreamsGetter getterForId4;
     @Mock
     private StateManager mockedStateManager;
+    @Mock
+    private DatastreamsGettersFinder mockedDatastreamsGettersFinder;
     @InjectMocks
     private OperationGetDeviceParametersImpl testGetDeviceParametersImpl;
 
@@ -65,12 +75,32 @@ public class OperationGetDeviceParametersImplTest {
         OperationGetDeviceParameters.GetValue expectedValue2 =
                 new OperationGetDeviceParameters.GetValue(TEST_DATASTREAM_ID_2, null, OperationGetDeviceParameters.Status.OK, TEST_AT_2, TEST_VALUE_2, null);
         OperationGetDeviceParameters.GetValue expectedValue3 =
-                new OperationGetDeviceParameters.GetValue(TEST_DATASTREAM_ID_3, TEST_FEED, OperationGetDeviceParameters.Status.NOT_FOUND, TEST_AT_3, null, null);
+                new OperationGetDeviceParameters.GetValue(TEST_DATASTREAM_ID_3, null, OperationGetDeviceParameters.Status.NOT_FOUND, 0, null, "No datastream getter found for this datastream");
         OperationGetDeviceParameters.GetValue expectedValue4 =
                 new OperationGetDeviceParameters.GetValue(TEST_DATASTREAM_ID_4, TEST_FEED, OperationGetDeviceParameters.Status.PROCESSING_ERROR, TEST_AT_4, null, TEST_ERROR);
 
-        when(mockedStateManager.getDatastreamsInformation(anyString(), any()))
-                .thenReturn(CompletableFuture.completedFuture(TEST_DATASTREAM_VALUES));
+        DatastreamsGettersFinder.Return getters = new DatastreamsGettersFinder.Return(Arrays.asList(getterForId1, getterForId2, getterForId4), asSet(TEST_DATASTREAM_ID_3));
+        when(getterForId1.getDevicesIdManaged()).thenReturn(TEST_DEVICE_ID_LIST);
+        when(getterForId2.getDevicesIdManaged()).thenReturn(TEST_DEVICE_ID_LIST);
+        when(getterForId4.getDevicesIdManaged()).thenReturn(TEST_DEVICE_ID_LIST);
+        when(getterForId1.getDatastreamIdSatisfied()).thenReturn(TEST_DATASTREAM_ID_1);
+        when(getterForId2.getDatastreamIdSatisfied()).thenReturn(TEST_DATASTREAM_ID_2);
+        when(getterForId4.getDatastreamIdSatisfied()).thenReturn(TEST_DATASTREAM_ID_4);
+        CompletableFuture<CollectedValue> futureForId1 = new CompletableFuture<>();
+        CompletableFuture<CollectedValue> futureForId2 = new CompletableFuture<>();
+        CompletableFuture<CollectedValue> futureForId4 = new CompletableFuture<>();
+        when(getterForId1.get(TEST_DEVICE_ID)).thenReturn(futureForId1);
+        when(getterForId2.get(TEST_DEVICE_ID)).thenReturn(futureForId2);
+        when(getterForId4.get(TEST_DEVICE_ID)).thenReturn(futureForId4);
+
+        CollectedValue value1 = new CollectedValue(TEST_AT_1, TEST_VALUE_1,null, null);
+        CollectedValue value2 = new CollectedValue(TEST_AT_2, TEST_VALUE_2,null, null);
+        CollectedValue value4 = new CollectedValue(TEST_AT_4, null,null, TEST_FEED);
+        futureForId1.complete(value1);
+        futureForId2.complete(value2);
+        futureForId4.complete(value4);
+        
+        when(mockedDatastreamsGettersFinder.getGettersSatisfying(any(), any())).thenReturn(getters);
 
         CompletableFuture<OperationGetDeviceParameters.Result> future =
                 testGetDeviceParametersImpl.getDeviceParameters(TEST_DEVICE_ID, TEST_DATASTREAMS);
@@ -83,6 +113,6 @@ public class OperationGetDeviceParametersImplTest {
         assertTrue(actualValues.contains(expectedValue2));
         assertTrue(actualValues.contains(expectedValue3));
         assertTrue(actualValues.contains(expectedValue4));
-        verify(mockedStateManager).getDatastreamsInformation(eq(TEST_DEVICE_ID), eq(TEST_DATASTREAMS));
+        verify(mockedStateManager).publishValues(TEST_EVENTS_VALUES);
     }
 }
