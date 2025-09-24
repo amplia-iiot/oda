1bc4f6e35806b62431d2ab40eb5d019d3afe8a97
ruben-romano <ruben.romano@amplia.es>
2025-09-24 18:03:06 +0200

    [ODA-506] - disconnect snmp clients when stopping bundle

--- oda-hardware/snmp/src/main/java/es/amplia/oda/hardware/snmp/internal/SnmpClientManager.java
+++ oda-hardware/snmp/src/main/java/es/amplia/oda/hardware/snmp/internal/SnmpClientManager.java
@@ -3,25 +3,42 @@ package es.amplia.oda.hardware.snmp.internal;
 import es.amplia.oda.core.commons.snmp.SnmpClient;
 import es.amplia.oda.core.commons.utils.ServiceRegistrationManager;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class SnmpClientManager implements AutoCloseable {
 
     private final ServiceRegistrationManager<SnmpClient> serviceRegistrationManager;
+    private List<SnmpClient> currentSnmpClients = new ArrayList<>();
 
     public SnmpClientManager(ServiceRegistrationManager<SnmpClient> serviceRegistrationManager) {
         this.serviceRegistrationManager = serviceRegistrationManager;
     }
 
     public void loadConfiguration(List<SnmpClient> snmpClients) {
+        disconnectClients();
+        this.currentSnmpClients.clear();
         serviceRegistrationManager.unregister();
-        for (SnmpClient client : snmpClients) {
+
+        this.currentSnmpClients = snmpClients;
+        for (SnmpClient client : this.currentSnmpClients) {
             serviceRegistrationManager.register(client);
         }
     }
 
     @Override
     public void close() {
+        disconnectClients();
         serviceRegistrationManager.unregister();
+    }
+
+    private void disconnectClients() {
+        if (this.currentSnmpClients == null) {
+            return;
+        }
+
+        for (SnmpClient client : this.currentSnmpClients) {
+            client.disconnect();
+        }
     }
 }
